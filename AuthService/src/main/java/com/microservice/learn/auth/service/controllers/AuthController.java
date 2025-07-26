30cfa3bd1c58ed14d313c3092ddbab7175db8a31
Shubrato Debnath <shubratodn44985@gmail.com>
2025-07-26 12:54:06 +0600

    Added AUTH Service

--- AuthService/src/main/java/com/microservice/learn/auth/service/controllers/AuthController.java
+++ AuthService/src/main/java/com/microservice/learn/auth/service/controllers/AuthController.java
@@ -1,4 +1,15 @@
 package com.microservice.learn.auth.service.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @GetMapping("/")
+    public String login() {
+        return "Welcome to Auth Service";
+    }
 }
